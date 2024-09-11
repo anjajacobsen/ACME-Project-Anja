501a5a21ad67fe80839845cb1e9f2a12b23c5089
phambt <brandon.pham22@gmail.com>
2024-09-10 23:36:34 -0400

    obtaining metrics

--- src/main.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node 
-//got above line from ChatGPT [1]
-//get the mode from ./run {input}
-var input_args = process.argv.slice(2); //gets user arguments pass in from run bash script REF: [2]
-var mode = input_args.length > 0 ? input_args[0] : "test"; //if no mode is passed in, default to test
-console.log(mode);
